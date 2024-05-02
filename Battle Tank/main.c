8a29f7de7d0129fdc80a89d8b0fe142cf116ebe6
jjana117 <167134220+jjana117@users.noreply.github.com>
2024-05-02 17:35:59 +0200

    main.c

--- Battle Tank/main.c
+++ Battle Tank/main.c
@@ -1,10 +1,13 @@
-#include <SDL.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SDL.h>
 
-#define SIZE 600
+#define SIZE 300
 
 static char curr_tank[50] = "images/tank1.bmp";
+int score = 0, lives = 2, tanks_left = 15, N = 15;
+Uint32 last_shot = 0, shoot_cooldown = 500;
+SDL_Rect tank;
 
 void generate_map(SDL_Renderer *renderer, int N, int **map, int tile_size){
 
@@ -99,54 +102,69 @@ void make_map(int N, int **map){
     map[N / 2 - 1][N - 3] = map[N / 2 + 1][N - 3] = 2;
 }
 
-void move_tank(SDL_Renderer *renderer, int N, SDL_Rect *tank, int **map, int dir){
+void move_tank(SDL_Renderer *renderer, int N, int **map, int dir){
     SDL_Surface *tank_s = SDL_LoadBMP(curr_tank);
     SDL_Texture *tank_t = SDL_CreateTextureFromSurface(renderer, tank_s);
 
-    int x = tank->x / tank->w;
-    int y = tank->y / tank->w;
+    int x = tank.x / tank.w;
+    int y = tank.y / tank.w;
 
     SDL_Rect select_tile;
     select_tile.x = 0;
     select_tile.y = 0;
-    select_tile.w = tank->w;
-    select_tile.h = tank->w;
+    select_tile.w = tank.w;
+    select_tile.h = tank.w;
 
     switch (dir) {
         case 0: //levo
-            strcpy(curr_tank, "images/tank0.bmp");
             if (x > 0 && map[x - 1][y] > 1 && map[x - 1][y] < 7) {
-                tank->x = tank->x - tank->w;
+                tank.x = tank.x - tank.w;
             }
             break;
         case 1: //gore
-            strcpy(curr_tank, "images/tank1.bmp");
             if (y > 0 && map[x][y - 1] > 1 && map[x][y - 1] < 7) {
-                tank->y = tank->y - tank->w;
+                tank.y = tank.y - tank.w;
             }
             break;
         case 2: //desno
-            strcpy(curr_tank, "images/tank2.bmp");
             if (x < N - 1 && map[x + 1][y] > 1 && map[x + 1][y] < 7) {
-                tank->x = tank->x + tank->w;
+                tank.x = tank.x + tank.w;
             }
             break;
         case 3: //dole
-            strcpy(curr_tank, "images/tank3.bmp");
             if (y < N - 1 && map[x][y + 1] > 1 && map[x][y + 1] < 7) {
-                tank->y = tank->y + tank->w;
+                tank.y = tank.y + tank.w;
             }
             break;
         default:
             break;
     }
-    SDL_RenderCopy(renderer, tank_t, &select_tile, tank);
+    SDL_RenderCopy(renderer, tank_t, &select_tile, &tank);
 
     SDL_FreeSurface(tank_s);
     SDL_DestroyTexture(tank_t);
 }
+void game_over(){
+    // ovde treba da se prekine igra i ukljuci pocetni meni, prikazu high score - ovi...
+    // to cemo kasnije
+}
+void kill_enemy(int i, int j, SDL_Renderer *renderer, int **map, int **enemies){
+    score += enemies[i][j] * 100;
+    enemies[i][j] = 0;
+    tanks_left--;
+}
+void kill_player(int n, int tile_size){
+    if(!lives){
+        game_over();
+    }
+    else{
+        lives--;
+        tank.x = n / 2 * tile_size;
+        tank.y = (n - 3) * tile_size;
+    }
+}
 
-void shoot(SDL_Renderer *renderer, int N, int **bullets, int tile_size, int **map){
+void shoot(SDL_Renderer *renderer, int N, int **bullets, int tile_size, int **map, int **enemies){
     SDL_Surface *bullet_s = SDL_LoadBMP("images/t4.bmp");
     SDL_Texture *bullet_t = SDL_CreateTextureFromSurface(renderer, bullet_s);
     SDL_FreeSurface(bullet_s);
@@ -172,15 +190,37 @@ void shoot(SDL_Renderer *renderer, int N, int **bullets, int tile_size, int **ma
         for (int j = 0; j < N; j++){
             switch(bullets[i][j]){
                 case 1:
+                    if(i > 0 && enemies[i - 1][j]){
+                        kill_enemy(i - 1, j, renderer, map, enemies);
+                    }
+                    else if(i - 1 ==  tank.x && j == tank.y){
+                        kill_player(N, tile_size);
+                    }
                     SDL_RenderCopy(renderer, bullet_t, &select_tile, &tile[i][j]);
-                    if (i > 0 && !(map[i - 1][j] >= 7 && map[i - 1][j] <= 10))
-                        bullets[i - 1][j] = 1;
+                    if (i > 0 && !(map[i - 1][j] >= 7 && map[i - 1][j] <= 10)) {
+                        if (!bullets[i - 1][j]) bullets[i - 1][j] = 1;
+                        else bullets[i - 1][j] = 0;
+                    }
+                    else if (i > 0 && (map[i - 1][j] == 7 || map[i - 1][j] == 8 || map[i - 1][j] == 9)) {
+                        map[i - 1][j] = 2;
+                    }
                     bullets[i][j] = 0;
                     break;
                 case 2:
+                    if(j > 0 && enemies[i][j - 1]){
+                        kill_enemy(i, j - 1, renderer, map, enemies);
+                    }
+                    else if(i ==  tank.x && j - 1 == tank.y){
+                        kill_player(N, tile_size);
+                    }
                     SDL_RenderCopy(renderer, bullet_t, &select_tile, &tile[i][j]);
-                    if (j > 0 && !(map[i][j - 1] >= 7 && map[i][j - 1] <= 10))
-                        bullets[i][j - 1] = 2;
+                    if (j > 0 && !(map[i][j - 1] >= 7 && map[i][j - 1] <= 10)) {
+                        if (!bullets[i][j - 1]) bullets[i][j - 1] = 2;
+                        else bullets[i][j - 1] = 0;
+                    }
+                    else if (j > 0 && (map[i][j - 1] == 7 || map[i][j - 1] == 8 || map[i][j - 1] == 9)) {
+                        map[i][j - 1] = 2;
+                    }
                     bullets[i][j] = 0;
                     break;
             }
@@ -191,14 +231,36 @@ void shoot(SDL_Renderer *renderer, int N, int **bullets, int tile_size, int **ma
             switch(bullets[i][j]){
                 case 3:
                     SDL_RenderCopy(renderer, bullet_t, &select_tile, &tile[i][j]);
-                    if (i < N - 1 && !(map[i + 1][j] >= 7 && map[i + 1][j] <= 10))
-                        bullets[i + 1][j] = 3;
+                    if(i < N - 1 && enemies[i + 1][j]){
+                        kill_enemy(i+1, j, renderer, map, enemies);
+                    }
+                    else if(i+1 ==  tank.x && j == tank.y){
+                        kill_player(N, tile_size);
+                    }
+                    else if (i < N - 1 && !(map[i + 1][j] >= 7 && map[i + 1][j] <= 10)) {
+                        if (!bullets[i + 1][j]) bullets[i + 1][j] = 3;
+                        else bullets[i + 1][j] = 0;
+                    }
+                    else if (i < N - 1 && (map[i + 1][j] == 7 || map[i + 1][j] == 8 || map[i + 1][j] == 9)) {
+                        map[i + 1][j] = 2;
+                    }
                     bullets[i][j] = 0;
                     break;
                 case 4:
                     SDL_RenderCopy(renderer, bullet_t, &select_tile, &tile[i][j]);
-                    if (j < N - 1 && !(map[i][j + 1] >= 7 && map[i][j + 1] <= 10))
-                        bullets[i][j + 1] = 4;
+                    if(j < N - 1 && enemies[i][j+1]){
+                        kill_enemy(i, j+1, renderer, map, enemies);
+                    }
+                    else if(i ==  tank.x && j+1 == tank.y){
+                        kill_player(N, tile_size);
+                    }
+                    if (j < N - 1 && !(map[i][j + 1] >= 7 && map[i][j + 1] <= 10)){
+                        if(!bullets[i][j + 1]) bullets[i][j + 1] = 4;
+                        else bullets[i][j + 1] = 0;
+                    }
+                    else if (j < N - 1 && (map[i][j + 1] == 7 || map[i][j + 1] == 8 || map[i][j + 1] == 9)) {
+                        map[i][j + 1] = 2;
+                    }
                     bullets[i][j] = 0;
                     break;
             }
@@ -215,8 +277,6 @@ int main(int argc, char* argv[]) {
         perror("Greska u inicijalizaciji.");
         return 1;
     }
-
-    int N = 15; //dimenzije mape koje treba da se unose preko menija
 
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
@@ -251,6 +311,11 @@ int main(int argc, char* argv[]) {
         map[i] = calloc(N, sizeof (int));
     make_map(N, map);
 
+    int **enemies;
+    enemies = calloc(N, sizeof (int*));
+    for (int i = 0; i < N; i++)
+        enemies[i] = calloc(N, sizeof (int));
+
     int **bullets;
     bullets = calloc(N, sizeof (int*));
     if (!bullets) {
@@ -263,7 +328,6 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < N; i++)
         bullets[i] = calloc(N, sizeof (int));
 
-    SDL_Rect tank;
     tank.x = N / 2 * tile_size;
     tank.y = (N - 3) * tile_size;
     tank.w = tile_size;
@@ -280,29 +344,37 @@ int main(int argc, char* argv[]) {
             else if (event.type == SDL_KEYDOWN){
                 if (event.key.keysym.sym == SDLK_LEFT){
                     dir = 0;
+                    strcpy(curr_tank, "images/tank0.bmp");
                 }
                 else if (event.key.keysym.sym == SDLK_UP){
                     dir = 1;
+                    strcpy(curr_tank, "images/tank1.bmp");
                 }
                 else if (event.key.keysym.sym == SDLK_RIGHT){
                     dir = 2;
+                    strcpy(curr_tank, "images/tank2.bmp");
                 }
                 else if (event.key.keysym.sym == SDLK_DOWN){
                     dir = 3;
+                    strcpy(curr_tank, "images/tank3.bmp");
                 }
                 else if (event.key.keysym.sym == SDLK_SPACE){
-                    int x = tank.x / tank.w;
-                    int y = tank.y / tank.w;
-                    bullets[x][y] = curr_tank[11] - '0' + 1;
+                    Uint32 curr_time = SDL_GetTicks();
+                    if (curr_time - last_shot > shoot_cooldown) {
+                        int x = tank.x / tank.w;
+                        int y = tank.y / tank.w;
+                        bullets[x][y] = curr_tank[11] - '0' + 1;
+                        last_shot = curr_time;
+                    }
                 }
             }
         }
         generate_map(renderer, N, map, tile_size);
-        shoot(renderer, N, bullets, tile_size, map);
-        move_tank(renderer, N, &tank, map, dir);
+        shoot(renderer, N, bullets, tile_size, map, enemies);
+        move_tank(renderer, N, map, dir);
         dir = -1;
         SDL_RenderPresent(renderer);
-        SDL_Delay(30);
+        SDL_Delay(20);
     }
 
     for (int i = 0; i < N; i++)
